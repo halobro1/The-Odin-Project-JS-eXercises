0b293c72b01aeed63298ef6ab2aa3d7c7fa58266
songying <gsy1911a1@gmail.com>
2022-07-15 18:30:04 +0800

    git commit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-    
+const palindromes = function (string) {
+//  1.
+    const stringLength = string.length;
+
+//  2. 
+    for(let i = 0; i =< stringLength / 2; i++)
+
 };
+
+// 1. Find the length of a string
+// 2. Loop through both halves of the string 
+// 3. Check if both halves are the same 
 
 // Do not edit below this line
 module.exports = palindromes;
