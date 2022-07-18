3936a0e55a5ea5b66e14f1a1213a02ec97249cf5
songying <gsy1911a1@gmail.com>
2022-07-19 00:07:51 +0800

    git commit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,37 @@
-const palindromes = function (string) {
-//  1.
-    const stringLength = string.length;
-
-//  2. 
-    for(let i = 0; i =< stringLength / 2; i++)
-
-//  3.
-    
+//suggested solution - 0/6 passed 6/6 failed
+const palindromes = function(string) {
+    processedString = string.toLowerCase().replace(/[^a-z]g, "");
+    return {
+        processedString
+        .split("")
+        .reverse()
+        .join("") == processedString
+    };
 };
 
-// 1. Find the length of a string
-// 2. Loop through both halves of the string 
-// 3. Check if both halves are the same 
+// Created Solution - 4/6 passed 2/6 failed:
+// const palindromes = function(string) {
+
+//     processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+
+//     //convert string into array
+//     let arrayValues = processedString.split('');
+    
+//     //reverse the array values
+//     let reverseArrayValues = arrayValues.reverse();
+
+//     //convert array back into string
+//     let reverseString = reverseArrayValues.join('');
+
+//     if (reverseString === string) {
+//         //if the string in reversed form is not the same as the string
+//         return false;
+//     } else{
+//         //if the string in reversed form is the same as the string:
+//         return true;
+//     }
+// }
+
 
 // Do not edit below this line
 module.exports = palindromes;
