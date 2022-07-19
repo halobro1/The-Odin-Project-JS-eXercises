727287a1ec33f8b1d71881ce17c7abe01b5a968c
songying <gsy1911a1@gmail.com>
2022-07-19 18:27:50 +0800

    git commit

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(result, length) {
+      var no1 = result[0],
+          no2 = result[1],
+          //the number that comes next in the sequence
+          next, 
+          //counts the numbers involved in the sequence
+          cnt = 2;
 
+    //while loop
+    while (cnt < length) {
+        next = no1 + no2;
+        no1 = no2;
+        no2 = next;
+        result.push(next);
+        cnt++;
+    }
+    return result;
 };
+
+ 
 
 // Do not edit below this line
 module.exports = fibonacci;
